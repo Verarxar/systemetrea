23385399ef89fa68b39181f26ef3be14d9092e40
Per Johansson <per.johansson@bimobject.com>
2018-07-23 16:45:12 +0200

    add article-detail component

--- src/app/articles/articles-routing.module.ts
+++ src/app/articles/articles-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ArticlesComponent } from './articles.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: ArticlesComponent }
+  { path: '', pathMatch: 'full', component: ArticlesComponent },
+  { path: ':id', pathMatch: 'full', component: ArticleDetailComponent }
 ];
 
 @NgModule({
--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from '../shared/shared.module';
 import { ArticlesRoutingModule } from './articles-routing.module';
 import { ArticlesComponent } from './articles.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 @NgModule({
   imports: [
@@ -11,6 +12,6 @@ import { ArticleListComponent } from './article-list/article-list.component';
     SharedModule
   ],
   providers: [],
-  declarations: [ArticlesComponent, ArticleListComponent]
+  declarations: [ArticlesComponent, ArticleListComponent, ArticleDetailComponent]
 })
 export class ArticlesModule { }
